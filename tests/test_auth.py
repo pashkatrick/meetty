4099c718ad125c06e3da06616c08cb15210f93bf
pashkatrick <coolbatmanarrow@gmail.com>
2022-03-25 21:49:56 +0300

    :sparkles: [quality] add tests

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,24 +1,34 @@
 import requests
 from config import host
+from random import randint
+import pytest
 
 
-class TestUtiles:
+class TestUtiles():
 
+    login = f'johndoe-{randint(0, 100)}'
     payload = {
-        "login": "johndoe",
+        "login": login,
         "password": "1D1"
     }
 
     def test_sign_in_with_no_user(self):
         response = requests.post(f'{host}/auth/signin', json=self.payload)
         assert response.status_code == 200
-        assert response.json()['status']
+        assert response.json()['data'] == 'user doesn\'t exist'
 
     def test_sign_up(self):
         response = requests.post(f'{host}/auth/signup', json=self.payload)
         assert response.status_code == 200
+        assert response.json()['status'] == f'user {self.login} was registered'
 
     def test_sign_in_with_user(self):
         response = requests.post(f'{host}/auth/signin', json=self.payload)
         assert response.status_code == 200
-        assert response.json()['access_token']
+        assert response.json()['token']
+        pytest.access_token = response.json()['token']
+
+    def test_access_token(self):
+        response = requests.get(
+            f'{host}/ready', headers={'Authorization': f'Bearer {pytest.access_token}'})
+        assert response.json()['status'] == f'ok, {self.login}'
--- tests/test_common.py
+++ tests/test_common.py
@@ -6,4 +6,4 @@ class TestUtiles:
 
     def test_utils(self):
         assert requests.get(f'{host}/').status_code == 200
-        assert requests.get(f'{self.host}/ready').status_code == 200
+        # assert requests.get(f'{self.host}/ready').status_code == 200
